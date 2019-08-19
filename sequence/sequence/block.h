d1a36d1b1b0fe2b38ffb368e147f727310aae8c7
suvorovroman <suvorovroman@gmail.com>
2019-08-19 19:19:59 +0300

    Коментарий структуры блока

--- sequence/sequence/block.h
+++ sequence/sequence/block.h
@@ -1,12 +1,15 @@
 #include "defs.h"
 
+/**	Блок последовательности.
+*/
 struct block
 {
+	/** Слово состояния блока. */
 	struct
 	{
-		unsigned int size : 4;
-		unsigned int offset_of_free_space : 16;
-	} descriptor;
-	struct block* next;
-	void* data;
+		unsigned int size : 4;	/**< Степень 2 для размера блока. */
+		unsigned int offset_of_free_space : 16;	/**< Смещение от начала блока до первого свободного байта. */
+	} descriptor;	
+	struct block* next;	/**< Указатель на следующий блок. */
+	void* data;	/**< Данные блока. */
 };
