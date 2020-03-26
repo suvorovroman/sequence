72d17e5c59aa43cf93c8b496ef4e64b2e9e5e26a
suvorovroman <suvorovroman@gmail.com>
2020-03-26 13:40:13 +0300

    Update block.c

--- sequence/sequence/block.c
+++ sequence/sequence/block.c
@@ -8,5 +8,6 @@ struct block* chain_block(struct block *_c, unsigned int _s, xlabel _x)
   b->previous = _c;
   b->state.exponent_of_size = evaluate_inital_size(_s);
   b->state.free_space_offset = 0;
+  b->value = memory_allocate(2 << b->state.exponent_of_size, _x);
   return b;
 }
