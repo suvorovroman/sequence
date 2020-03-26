06fd27ecd2e51849c02480f2f515c1957fecfd6f
suvorovroman <suvorovroman@gmail.com>
2020-03-26 13:31:56 +0300

    Update block.c

--- sequence/sequence/block.c
+++ sequence/sequence/block.c
@@ -1,1 +1,11 @@
+#include "block.h"
 
+struct block* chain_block(struct block *_c, unsigned int _s, xlabel _x)
+{
+  struct block* b;
+  
+  b = memory_allocate(sizeof(struct block), _x);
+  b->previous = _c;
+  
+  return b;
+}
